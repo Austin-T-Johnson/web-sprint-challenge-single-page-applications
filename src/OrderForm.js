5739169bc14f6ba53b33ff2e3a7f0a6b4dd5d302
Austin Johnson <austintjohnson92@gmail.com>
2022-04-01 15:27:25 -0700

    Sixth test passing

--- src/OrderForm.js
+++ src/OrderForm.js
@@ -3,20 +3,29 @@ import { useHistory } from "react-router-dom";
 
 export default function OrderForm() {
 
-const onChange = evt => {
-    const name = evt.target.name;
-}
+    const onChange = evt => {
+        const name = evt.target.name;
+    }
 
     return (
         <form id="pizza-form">
-        <input 
-        id="name-input"
-        type="text"
-        placeholder="Enter your name"
-        onChange={onChange}
-        />
-            
-        
+            <input
+                id="name-input"
+                type="text"
+                placeholder="Enter your name"
+                onChange={onChange}
+            />
+
+            <label>Choice of size
+                <select id="size-dropdown">
+                    <option value="">-- Select size of pizza --</option>
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option value="large">Large</option>
+                    <option value="extra-large">Extra Large</option>
+                </select>
+
+            </label>
 
 
 
