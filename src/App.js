3474806507b6274c072e0aa2ed067af21c5a96c0
Austin Johnson <austintjohnson92@gmail.com>
2022-04-01 14:51:42 -0700

    First test passing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Route, Link, Switch } from "react-router-dom";
+import Home from './Home';
 
 const App = () => {
+
+
+
+
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <p>You're favorite food delivered while coding</p>
+      
+      <div className='nav-links'>
+        <Link to="/">Home</Link>
+        <Link>Order</Link>
+      </div>
+
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+
+    </Switch>
     </>
+
   );
 };
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+export default function Home() {
+    const history = useHistory();
+
+    return (
+        <div className='home-wrapper'>
+            <img
+                className='home-image'
+                src='../Assets/Pizza.jpg'
+                alt=''
+            />
+            <button
+                // onClick={routeToOrder}
+                id="order-pizza"
+            >
+                Order now!
+            </button>
+        </div>
+    )
+}
