5c45c227593ec7a1c8a3dc560acfe8d02247350d
Austin Johnson <austintjohnson92@gmail.com>
2022-04-02 10:26:41 -0700

    Final Solution in place, need to style

--- src/App.js
+++ src/App.js
@@ -5,23 +5,53 @@ import Home from './Home';
 import OrderForm from './OrderForm';
 
 const initialFormValues = {
-  name: ''
+  name: '',
+  size: '',
+  instructions: '',
+  pepperoni: false,
+  sausage: false,
+  spicyItalianSausage: false,
+  grilledChicken: false
 }
+
+const initialPizzaOrders = ""
 
 const App = () => {
   const [formValues, setFormValues] = useState(initialFormValues)
   const [formError, setFormError] = useState("");
+  const [pizzaOrders, setPizzaOrders] = useState(initialPizzaOrders)
 
+
+  const postPizzaOrder = newPizzaOrder => {
+    axios.post("https://reqres.in/api/orders", newPizzaOrder)
+      .then(res => {
+        console.log(res.data)
+        setPizzaOrders(res.data)
+      }).catch(err => console.error(err))
+      .finally(() => setFormValues(initialFormValues))
+
+  }
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({ ...formValues, [inputName]: inputValue });
+  }
 
   const submitForm = () => {
-  const newOrder = {
-    name: formValues.name.trim()
+
+    const newPizzaOrder = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      instructions: formValues.instructions.trim(),
+      toppings: [
+        "pepperoni",
+        "sausage",
+        "spicy italian sausage",
+        "grilled chicken"
+      ]
+        .filter(topping => !!formValues[topping])
+    }
+    postPizzaOrder(newPizzaOrder);
   }
-  if (!newOrder.name) {
-    setFormError("name must be at least 2 characters")
-    return;
-  }
-}
   return (
     <>
       <h1>Lambda Eats</h1>
@@ -34,7 +64,7 @@ const App = () => {
       <p className="error">{formError}</p>
       <Switch>
         <Route path="/pizza">
-          <OrderForm />
+          <OrderForm submit={submitForm} values={formValues} update={updateForm} setFormError={setFormError} />
         </Route>
 
         <Route path="/">
--- src/OrderForm.js
+++ src/OrderForm.js
@@ -1,34 +1,53 @@
 import React from 'react';
-import { useHistory } from "react-router-dom";
+
 
 export default function OrderForm(props) {
     const {
         values,
         submit,
-        change,
-        disabled,
-        errors,
+        update,
+        change
     } = props
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
     const onChange = evt => {
-        const name = evt.target.name;
+        const { name, value, checked, type } = evt.target;
+       const valueToUse = type === "checkbox" ? checked : value
+        update(name, valueToUse);
+       
+        console.log(name, value)
+
+        if (name === "name") {
+            if (value.length < 2) {
+                props.setFormError("name must be at least 2 characters")
+                return;
+            } else {
+                props.setFormError("")
+            }
+        }
     }
 
     return (
 
         // name input //
 
-        <form id="pizza-form">
+        <form id="pizza-form" onSubmit={onSubmit}>
             <input
                 id="name-input"
                 type="text"
                 placeholder="Enter your name"
                 onChange={onChange}
+                value={values.name}
+                name="name"
             />
 
             {/* Size of pizza choice */}
 
             <label>Choice of size
-                <select id="size-dropdown">
+                <select id="size-dropdown" value={values.size} name="size" onChange={onChange}>
                     <option value="">-- Select size of pizza --</option>
                     <option value="small">Small</option>
                     <option value="medium">Medium</option>
@@ -45,21 +64,33 @@ export default function OrderForm(props) {
             <label>Pepperoni
                 <input
                     type="checkbox"
+                    name="pepperoni"
+                    onChange={onChange}
+                    checked={values.pepperoni}
                 />
             </label>
             <label>Sausage
                 <input
                     type="checkbox"
+                    name="sausage"
+                    onChange={onChange}
+                    checked={values.sausage}
                 />
             </label>
             <label>Spicy Italian Sausage
                 <input
                     type="checkbox"
+                    name="spicyItalianSausage"
+                    onChange={onChange}
+                    checked={values.spicyItalianSausage}
                 />
             </label>
             <label>Grilled Chicken
                 <input
                     type="checkbox"
+                    name="grilledChicken"
+                    onChange={onChange}
+                    checked={values.grilledChicken}
                 />
             </label>
 
@@ -67,12 +98,17 @@ export default function OrderForm(props) {
 
             <h3>Special Instructions</h3>
 
-            <input 
-            id="special-text"
-            type="text"
-            placeholder="Any special instructions?"
+            <input
+                id="special-text"
+                type="text"
+                placeholder="Any special instructions?"
+                name="instructions"
+                value={values.instructions}
+                onChange={onChange}
             />
 
+            {/* Submit Button */}
+            <button id="order-button">Submit Order</button>
 
         </form>
 
